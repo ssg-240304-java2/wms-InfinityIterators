9a0947d4f0deb410a31d941a8bf385ad55dc20fc
dmlwls12 <gimuijin572@gmail.com>
2024-05-18 00:38:06 +0900

    #13 feat: Add main menu functionality with three options
    - View the full menu
    - Receiving Inventory
    - Shipping Inventory

--- src/main/java/com/infinityiterators/receipt/Controller/ReiceiptController.java
+++ src/main/java/com/infinityiterators/receipt/Controller/ReiceiptController.java
@@ -1,4 +1,12 @@
 package com.infinityiterators.receipt.Controller;
 
 public class ReiceiptController {
+    public void selectAllStock() {
+    }
+
+    public void selectStockIn() {
+    }
+
+    public void selectOutOfStock() {
+    }
 }
