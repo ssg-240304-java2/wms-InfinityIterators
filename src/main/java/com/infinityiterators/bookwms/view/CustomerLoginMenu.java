f17141c1a64cd4f9549a169769aca0856f8dc429
Awesome <awesome_devnet@outlook.com>
2024-05-18 11:23:22 +0900

    #33 feat(account): add customer login/signup views

---
+++ src/main/java/com/infinityiterators/bookwms/view/CustomerLoginMenu.java
@@ -0,0 +1,100 @@
+package com.infinityiterators.bookwms.view;
+
+import com.infinityiterators.bookwms.account.User;
+import com.infinityiterators.bookwms.utils.interaction.*;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.format.*;
+
+public class CustomerLoginMenu {
+    public void displayMenu() {
+
+        while(true) {
+            Console.clear();
+            Menu.displayMenuHeader("로그인");
+            Menu.displaySelectionMenu("로그인", "회원가입", "메인 메뉴");
+
+            switch(Input.requestInt("메뉴를 선택하세요")) {
+                case 1:
+                    User login = login();
+                    new CustomerMenu().displayMenu(login);
+                    break;
+                case 2:
+                    signUp();
+                    break;
+                case 3:
+                    return;
+                default:
+                    Console.printError("잘못된 입력입니다.");
+                    break;
+            }
+        }
+    }
+
+    private User login() {
+        Console.clear();
+        Menu.displayMenuHeader("로그인");
+
+        String id = Input.requestString("아이디를 입력하세요");
+        String password = Input.requestString("비밀번호를 입력하세요");
+
+        // todo. validate id and password
+        // todo. return user
+
+        return null;
+    }
+
+    private void signUp() {
+        Console.clear();
+        Menu.displayMenuHeader("회원가입");
+
+        String id = "";
+        boolean isIdDuplicated = true;
+        do {
+            id = Input.requestString("아이디를 입력하세요");
+            // todo. check id duplication
+        } while(!isIdDuplicated);
+
+        String password = Input.requestString("비밀번호를 입력하세요");
+
+        final int PASSWORD_CHECK_LIMIT = 5;
+        int passwordCheckCount = 0;
+        while(true) {
+            String passwordCheck = Input.requestString("비밀번호를 다시 입력하세요");
+            if(password.equals(passwordCheck)) break;
+            else {
+                Console.printError("비밀번호가 일치하지 않습니다.");
+                passwordCheckCount++;
+                if(passwordCheckCount >= PASSWORD_CHECK_LIMIT) {
+                    Console.printError("비밀번호 확인 횟수 초과 : " + passwordCheckCount + "/" + PASSWORD_CHECK_LIMIT + "회");
+                    Console.print("회원가입을 종료합니다.", DisplayType.SYSTEM, true);
+                    return;
+                }
+            }
+        }
+
+        String name = Input.requestString("이름을 입력하세요");
+        String phoneNumber = Input.requestString("전화번호를 입력하세요");
+        String address = Input.requestString("주소를 입력하세요");
+        String dateOfBirth = Input.requestString("생년월일을 입력하세요 (yyyy-mm-dd)");
+        // yyyy-mm-dd 서식에 맞게 입력되었는지 검사
+        // Date.valueOf(Input.requestString("생년월일을 입력하세요 (yyyy-mm-dd)"));
+        while(!isValidDate(dateOfBirth)) {
+            Console.printError("날짜 형식이 올바르지 않습니다.");
+            dateOfBirth = Input.requestString("생년월일을 입력하세요 (yyyy-mm-dd)");
+        }
+
+        // todo. save customer
+    }
+
+    private boolean isValidDate(String date) {
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            LocalDate.parse(date, formatter);
+            return true;
+        } catch(DateTimeParseException e) {
+            return false;
+        }
+    }
+}
